97f3ec628b27641442de4638260b2c9539c0ea24
192111124 <114208869+192111124@users.noreply.github.com>
2023-08-26 12:21:22 +0530

    Add files via upload

---
+++ vignere cipher.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function to encrypt a message using Vigenère Cipher
+void vigenereEncrypt(char *key, char *message) {
+    int keyLength = strlen(key);
+    int messageLength = strlen(message);
+
+    printf("Encrypted message: ");
+
+    for (int i = 0, j = 0; i < messageLength; i++) {
+        char currentChar = message[i];
+        
+        if (isalpha(currentChar)) {
+            int keyShift = tolower(key[j % keyLength]) - 'a';
+            char encryptedChar;
+
+            if (isupper(currentChar)) {
+                encryptedChar = (currentChar - 'A' + keyShift) % 26 + 'A';
+            } else {
+                encryptedChar = (currentChar - 'a' + keyShift) % 26 + 'a';
+            }
+
+            printf("%c", encryptedChar);
+            j++;
+        } else {
+            printf("%c", currentChar);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    char key[100];
+    char message[100];
+
+    printf("Enter the key (alphabetic characters only): ");
+    scanf("%s", key);
+
+    printf("Enter the message to be encrypted: ");
+    scanf(" %[^\n]s", message);
+
+    vigenereEncrypt(key, message);
+
+    return 0;
+}
